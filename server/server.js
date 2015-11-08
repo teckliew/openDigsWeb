cd0cf3a55e62ea6c6550d411f1c06bc81cdc39f7
teckliew <teck@tekleemedia.com>
2015-11-07 19:23:35 -0800

    added video listings

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ Meteor.methods({
     this.unblock();
     try {
       var result = HTTP.call("GET", "https://zipcode-retsly.herokuapp.com/video");
-      return result.data;
+      return result.data
+      l;
     } catch (e) {
       // Got a network error, time-out or HTTP error in the 400 or 500 range.
       return false;
