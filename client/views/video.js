263dc34ee8f3457fa46ae4b5a6b86e25ede3fdb6
teckliew <teck@tekleemedia.com>
2015-11-08 01:06:44 -0800

    updated ui

--- client/views/video.js
+++ client/views/video.js
@@ -1,14 +1,28 @@
 Meteor.startup(function () {
   Meteor.call('videoList', function(error, data) {
+    var heroVid = {
+      title: data[0]['displayName'],
+      video: data[0]['video']['youtube_url'].split('v=')[1],
+      price: data[0]['originalPrice']
+    }
+
     Session.set('listings', data);
-    Session.set('selectedVideo', data[0]['video']['youtube_url']);
+    Session.set('heroVid', heroVid)
   });
 });
 
 Template.video.helpers({
   videoId: function() {
-    var ytLink = Session.get('selectedVideo');
-    var ytId = ytLink.split('v=')[1];
+    var ytLink = Session.get('heroVid');
+    var ytId = ytLink['video'];
     return ytId;
+  },
+  price: function() {
+    var data = Session.get('heroVid');
+    return data['price'];
+  },
+  title: function() {
+    var data = Session.get('heroVid');
+    return data['title'];
   }
 })
