45329951489c947a79baf9a794ea25f096ed6284
teckliew <teck@tekleemedia.com>
2016-01-05 10:55:58 -0800

    testing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 Meteor.methods({
   videoList: function () {
-    this.unblock();
     try {
       var result = HTTP.call("GET", "https://zipcode-retsly.herokuapp.com/video");
-      return result.data
-      l;
+      return result.data;
     } catch (e) {
       // Got a network error, time-out or HTTP error in the 400 or 500 range.
       return false;
